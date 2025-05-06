e812e08aef225407bf118b418a2c6bae60ebe9a7
Hatim Thayyil <hatim@thayyil.net>
2025-05-06 07:36:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# My personal Neovim configuration
+
+Personal Neovim configuration using [NixVim](https://github.com/nix-community/nixvim).
+
+## Usage
+
+To test this configuration, simply run the following command
+
+```
+nix run .
+```
--- flake.nix
+++ flake.nix
@@ -1,18 +1,32 @@
 {
-  description = "A very basic flake";
+  description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.follows = "nixvim/nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, nixvim, ... }:
-    let
-      config = {
-        colorschemes.gruvbox.enable = true;
-      };
-
+    {
+      nixvim,
+      flake-parts,
+      pre-commit-hooks,
+      treefmt-nix,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -20,19 +34,55 @@
         "aarch64-darwin"
       ];
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (
-        system:
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          system,
+          pkgs,
+          config,
+          ...
+        }:
         let
+          nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
-          nvim = nixvim'.makeNixvim config;
+          nixvimModule = {
+            inherit system; # or alternatively, set `pkgs`
+            module = import ./config; # import the module directly
+            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            extraSpecialArgs = {
+              # inherit (inputs) foo;
+            };
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
-          inherit nvim;
-          default = nvim;
-        }
-      );
+          # Use treefmt instead of a single formatter
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              statix.enable = true;
+            };
+          };
+
+          checks = {
+            # Run `nix flake check .` to verify that your config is not broken
+            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                statix.enable = true;
+              };
+            };
+          };
+
+          packages = {
+            # Lets you run `nix run .` to start nixvim
+            default = nvim;
+          };
+        };
     };
 }
